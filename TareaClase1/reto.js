e224d264875e776dca0fe9f23bbfb8ff37dc7ee5
JonnyLunaGuerrero <jonnylunag@gmail.com>
2024-09-24 11:05:39 -0500

    Tarea 1

---
+++ TareaClase1/reto.js
@@ -0,0 +1,21 @@
+//Si quieres retarte te invitamos a crear un programa que sea capaz de recorrer el array estudiantes de el punto 2 y encontrar a el/la estudiante con el mayor promedio, utilizando un bucle for o cualquer otro método visto en clase
+
+const estudiantes = [
+    { nombre: "Pedro", edad: 29, promedio: 7.9 },
+    { nombre: "Ana", edad: 33, promedio: 8.9 },
+    { nombre: "Pablo", edad: 32, promedio: 9.5 },
+    { nombre: "Juan", edad: 25, promedio: 6.0 },
+    { nombre: "Maria", edad: 28, promedio: 7.3 },
+    { nombre: "Andres", edad: 45, promedio: 8.7 },
+];
+
+let mayorPromedio = 0;
+for (let i = 0; i < estudiantes.length; i++) {
+    const estudiante = estudiantes[i];
+    if(mayorPromedio < estudiante.promedio){
+        mayorPromedio = estudiante.promedio
+    }
+    
+}
+
+console.log(mayorPromedio)
